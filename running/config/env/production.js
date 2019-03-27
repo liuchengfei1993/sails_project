256b4d52e300a4d0ed6b53ee1cb7ec46cc36a5d8
liuchengfei1993 <869275085@qq.com>
2019-03-27 11:47:42 +0800

    2

--- running/config/env/production.js
+++ running/config/env/production.js
@@ -250,10 +250,10 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+    onlyAllowOrigins: [
+      'https://47.52.158.139 ',
+      'https://whafd.online',
+    ],
 
 
     /***************************************************************************
